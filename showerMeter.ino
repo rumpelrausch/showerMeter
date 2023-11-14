94b9e3aad4870dfba53484a4d733e23b5320d34a
flp <flp@schicknemail.de>
2023-11-14 13:39:57 +0100

    feat: implements state machine

--- showerMeter.ino
+++ showerMeter.ino
@@ -1,14 +1,20 @@
 #include <Tiny4kOLED.h>
 
-#define PAGE_SWITCH_EVERY_SECONDS 5
-#define SECONDS_UNTIL_UNHAPPY 360
-#define SECONDS_UNTIL_WARN 420
 #define CENT_PER_KWH 36
 #define HEATER_KW 21
 #define DAYS_PER_YEAR 160
-#define OFF_AFTER_SECONDS 4
-#define FINISHED_AFTER_OFF_SECONDS 60
+
+#define OFF_AFTER_SECONDS 3
+#define FINISHED_AFTER_OFF_SECONDS 10
 #define ANALOG_THRESHOLD 65
+
+#define PAGE_SWITCH_EVERY_SECONDS 4
+#define SECONDS_UNTIL_UNHAPPY 360
+#define SECONDS_UNTIL_WARN 420
+
+#define GREET_MILLISECONDS 1000
+#define SLOW_POLL_MILLISECONDS 2003 // use a prime to avoid continously tracking zero crossings
+#define FAST_POLL_MILLISECONDS 307
 
 // ###############################################
 // ###############################################
@@ -20,18 +26,28 @@
 #define PAGE_TIME 2
 #define CENTICENT_PER_SECOND (CENT_PER_KWH * HEATER_KW) / 36
 
-uint8_t currentPage = PAGE_COST;
-unsigned long lastMillis = 0;
-volatile uint16_t deciSeconds = 0;
+#define STATE_IDLE 1
+#define STATE_ON 2
+#define STATE_WAIT_FOR_OFF 3
+#define STATE_WAIT_FOR_FINISH 4
+#define STATE_TO_IDLE 5
+
+volatile uint8_t state = STATE_IDLE;
+uint8_t currentPage;
+volatile uint8_t timer1value;
 volatile uint16_t seconds = 0;
-volatile uint16_t secondsOn = 0;
-volatile uint16_t timerOffDetection = 0;      // > 0: Timer is running
-volatile uint16_t timerFinishedDetection = 0; // > 0: Timer is running
-volatile uint8_t blinkState = 0;
-volatile uint16_t centiCents = 0;
+volatile uint16_t secondsOn;
+volatile uint16_t centiCents;
+volatile uint16_t timerOffDetection;      // > 0: Timer is running
+volatile uint16_t timerFinishedDetection; // > 0: Timer is running
+volatile uint8_t blinkState;
+volatile bool displayIsDirty;
+volatile uint8_t updateCount = 0;
 
 void setup()
 {
+  reset();
+  state = STATE_TO_IDLE;
   pinMode(ANALOG_PIN, INPUT);
 
   oled.begin(64, 48, sizeof(tiny4koled_init_64x48), tiny4koled_init_64x48);
@@ -40,27 +56,18 @@ void setup()
   oled.clear();
   oled.on();
   greet();
+  oled.clear();
 
+#ifndef TEST_ANALOG_IN
   setNextPage();
   initTimer();
-}
-
-void greet()
-{
-  oled.setContrast(255);
-  oled.setCursor(0, 1);
-  oled.print(F(" Shower "));
-  oled.setCursor(0, 4);
-  oled.print(F("  Check "));
-#ifndef TEST_ANALOG_IN
-  delay(5000);
 #endif
 }
 
 void loop()
 {
-  int analog = checkOnOff();
 #ifdef TEST_ANALOG_IN
+  int analog = analogRead(ANALOG_PIN);
   static char outString[] = "        ";
   itoa(analog, outString, 10);
   oled.setCursor(0, 0);
@@ -78,112 +85,168 @@ void loop()
   }
   delay(500);
 #else
-  delay(timerFinishedDetection == 0 ? 2000 : 100); // slow poll if off
+  state = determinePhase();
+  updateDisplay();
+  debugOnOff();
+  delay(state == STATE_IDLE ? SLOW_POLL_MILLISECONDS : FAST_POLL_MILLISECONDS); // slow poll if off
 #endif
 }
 
+void reset()
+{
+  currentPage = PAGE_COST;
+  timer1value = 0;
+  secondsOn = 0;
+  centiCents = 0;
+  timerOffDetection = 0;      // > 0: Timer is running
+  timerFinishedDetection = 0; // > 0: Timer is running
+  blinkState = 0;
+  displayIsDirty = true;
+}
+
+void greet()
+{
+  oled.setContrast(255);
+  oled.setCursor(0, 1);
+  oled.print(F(" Shower "));
+  oled.setCursor(0, 4);
+  oled.print(F("  Check "));
+#ifndef TEST_ANALOG_IN
+  delay(GREET_MILLISECONDS);
+#endif
+}
+
+#ifndef TEST_ANALOG_IN
+// 16 Hz
 void initTimer()
 {
-  noInterrupts();
-  // Clear registers
   TCNT1 = 0;
   TCCR1 = 0;
 
-  // 10.01602564102564 Hz (16000000/((194+1)*8192))
-  OCR1C = 194;
-  // interrupt COMPA
-  OCR1A = OCR1C;
-  // CTC
+  OCR1A = 244;
+  TCCR1 = 0x8D;
   TCCR1 |= (1 << CTC1);
-  // Prescaler 8192
-  TCCR1 |= (1 << CS13) | (1 << CS12) | (1 << CS11);
-  // Output Compare Match A Interrupt Enable
   TIMSK |= (1 << OCIE1A);
-  interrupts();
 }
+#endif
 
 #ifndef TEST_ANALOG_IN
 ISR(TIMER1_COMPA_vect)
 {
-  deciSeconds++;
-  if (deciSeconds == 10)
+  noInterrupts();
+  timer1value++;
+  if (timer1value % 8 == 0)
   {
-    deciSeconds = 0;
-    seconds++;
-    if (timerOffDetection > 0)
-    {
-      timerOffDetection++;
-      secondsOn++;
-      centiCents += CENTICENT_PER_SECOND;
-    }
-    if (timerFinishedDetection > 0)
-    {
-      timerFinishedDetection++;
-    }
-    updateDisplay();
+    tickBlink();
   }
-  if (deciSeconds % 5 == 0)
+  if (timer1value >= 16)
   {
-    blink();
+    timer1value = 0;
+    tickSecond();
   }
+  interrupts();
 }
 #endif
 
-int checkOnOff()
+void tickSecond()
 {
-  int analog = analogRead(ANALOG_PIN);
-  if (analog > ANALOG_THRESHOLD)
-  {
-    timerOffDetection = 1;
-    timerFinishedDetection = 0;
-    return analog;
-  }
-
-  if (timerOffDetection >= OFF_AFTER_SECONDS)
-  {
-    timerOffDetection = 0;
-    timerFinishedDetection = 1;
-  }
-
-  if (timerFinishedDetection > FINISHED_AFTER_OFF_SECONDS)
-  {
-    timerFinishedDetection = 0;
-    secondsOn = 0;
-    seconds = 0;
-    centiCents = 0;
-  }
-  return analog;
-}
-
-void updateDisplay()
-{
-  static bool wasOn = true;
-  if (secondsOn == 0)
-  {
-    if (wasOn)
-    {
-      wasOn = false;
-      oled.clear();
-      oled.setCursor(0, 0);
-      oled.print(F("."));
-      oled.setContrast(0);
-    }
-    return;
-  }
-
-  if (!wasOn && secondsOn > 0)
-  {
-    wasOn = true;
-    oled.clear();
-    oled.setContrast(255);
-    currentPage = PAGE_COST;
-    seconds = 0;
-  }
+  seconds++;
+  displayIsDirty = true;
 
   if (seconds % PAGE_SWITCH_EVERY_SECONDS == 0)
   {
     setNextPage();
   }
+
+  if (state == STATE_ON || state == STATE_WAIT_FOR_OFF)
+  {
+    secondsOn++;
+    centiCents += CENTICENT_PER_SECOND;
+  }
+
+  if (state == STATE_WAIT_FOR_OFF)
+  {
+    timerOffDetection++;
+  }
+
+  if (state == STATE_WAIT_FOR_FINISH)
+  {
+    timerFinishedDetection++;
+  }
+}
+
+uint8_t determinePhase()
+{
+  int analog = analogRead(ANALOG_PIN);
+
+  if (analog > ANALOG_THRESHOLD)
+  {
+    timerOffDetection = 0;
+    timerFinishedDetection = 0;
+    return STATE_ON;
+  }
+
+  // analog is below threshold
+
+  if (state == STATE_ON)
+  {
+    timerOffDetection = 0;
+    return STATE_WAIT_FOR_OFF;
+  }
+
+  if (state == STATE_WAIT_FOR_OFF && timerOffDetection >= OFF_AFTER_SECONDS)
+  {
+    timerFinishedDetection = 0;
+    return STATE_WAIT_FOR_FINISH;
+  }
+
+  if (state == STATE_WAIT_FOR_FINISH && timerFinishedDetection > FINISHED_AFTER_OFF_SECONDS)
+  {
+    reset();
+    return STATE_TO_IDLE;
+  }
+
+  // state has not changed
+  return state;
+}
+
+void updateDisplay()
+{
+  static uint8_t pageBefore;
+
+  // displayIsDirty is set once per second in tickSecond().
+  if (!displayIsDirty || state == STATE_IDLE)
+  {
+    return;
+  }
+  displayIsDirty = false;
+  updateCount++;
+
+  if (state == STATE_TO_IDLE)
+  {
+    oled.clear();
+    oled.setCursor(0, 0);
+    oled.print(F("."));
+    oled.setContrast(0);
+    state = STATE_IDLE;
+    return;
+  }
+
+  noInterrupts();
+
+  if (currentPage != pageBefore)
+  {
+    switch (currentPage)
+    {
+    case PAGE_COST:
+      beginCostPage();
+      break;
+    case PAGE_TIME:
+      beginTimePage();
+    }
+  }
+  pageBefore = currentPage;
+
   switch (currentPage)
   {
   case PAGE_COST:
@@ -192,15 +255,43 @@ void updateDisplay()
   case PAGE_TIME:
     updateTimePage();
   }
+  debugOnOff();
+  interrupts();
+}
 
+void debugOnOff()
+{
   oled.setCursor(56, 5);
-  // oled.print(timerOffDetection > 0 ? F("o") : F(" "));
   oled.startData();
-  oled.sendData(timerOffDetection > 0 ? 0b10000000 : 0);
+  oled.sendData(state == STATE_ON || state == STATE_TO_IDLE ? 0b10000000 : 0);
   oled.endData();
 }
 
-void blink()
+void debugPhase()
+{
+  oled.setCursor(56, 4);
+  char s[] = "  ";
+  itoa(state, s, 10);
+  oled.print(s);
+}
+
+void debugSecondsOn()
+{
+  oled.setCursor(48, 4);
+  char s[] = "    ";
+  itoa(secondsOn, s, 10);
+  oled.print(s);
+}
+
+void debugUpdateCount()
+{
+  oled.setCursor(48, 4);
+  char s[] = "    ";
+  itoa(updateCount, s, 10);
+  oled.print(s);
+}
+
+void tickBlink()
 {
   if (blinkState < 1)
   {
@@ -233,11 +324,9 @@ void setNextPage()
   {
   case PAGE_COST:
     currentPage = PAGE_TIME;
-    beginTimePage();
     break;
   case PAGE_TIME:
     currentPage = PAGE_COST;
-    beginCostPage();
   }
 }
 
@@ -279,6 +368,7 @@ char *getCentString(uint16_t cents)
 
 void beginCostPage()
 {
+  oled.setContrast(255);
   oled.clear();
   drawEuro(0, 0);
   oled.setCursor(0, 2);
@@ -289,6 +379,7 @@ void beginCostPage()
 void beginTimePage()
 {
   oled.clear();
+  // oled.setContrast(255);
   oled.setCursor(0, 0);
 }
 
